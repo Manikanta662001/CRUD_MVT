edf27a0bfaa72d844a65149aa5608cf7f9ab28b6
Manikanta662001 <gundlurimanikanta142@gmail.com>
2022-12-13 20:14:47 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from app.models import *
+from django.db.models.functions import Length
 # Create your views here.
 def display_topic(request):
     LTO=Topic.objects.all()
+    
     d={'LTO':LTO}
     return render(request,'display_topic.html',d)
 def display_webpage(request):
     LWO=Webpage.objects.all()
+    LWO=Webpage.objects.filter(topic_name='Cricket')
+    LWO=Webpage.objects.exclude(topic_name='Cricket')
+    LWO=Webpage.objects.all()[2:6:]
+    LWO=Webpage.objects.all().order_by('name')
+    LWO=Webpage.objects.filter(topic_name='Cricket').order_by('-name')
+    LWO=Webpage.objects.all().order_by(Length('name'))
+    LWO=Webpage.objects.all().order_by(Length('name').desc())
     d={'LWO':LWO}
     return render(request,'display_webpage.html',d)
 def display_access(request):
